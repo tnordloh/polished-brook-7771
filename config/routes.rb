78ce1e40c02c7e8451bc233423d7cf0065334405
Tim Nordloh <tnordloh@gmail.com>
2023-07-16 17:38:30 -0600

    Story 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/contestants/', to: 'contestants#index'
+
   get '/project/:id', to: 'projects#show'
 end
